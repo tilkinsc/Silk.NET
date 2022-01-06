bfe9983857b7f6fa0a39929812351e22246fffe9
Dylan Perks <dmp9biz@gmail.com>
2022-01-06 22:29:23 +0000

    Commit default Azure Function template

---
+++ src/Interactive/Silk.NET.Interactive.Azure/ExecuteSilkCSharp.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Silk.NET.Interactive.Azure
+{
+    public static class ExecuteSilkCSharp
+    {
+        [FunctionName("ExecuteSilkCSharp")]
+        public static async Task<List<string>> RunOrchestrator(
+            [OrchestrationTrigger] IDurableOrchestrationContext context)
+        {
+            var outputs = new List<string>();
+
+            // Replace "hello" with the name of your Durable Activity Function.
+            outputs.Add(await context.CallActivityAsync<string>("ExecuteSilkCSharp_Hello", "Tokyo"));
+            outputs.Add(await context.CallActivityAsync<string>("ExecuteSilkCSharp_Hello", "Seattle"));
+            outputs.Add(await context.CallActivityAsync<string>("ExecuteSilkCSharp_Hello", "London"));
+
+            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
+            return outputs;
+        }
+
+        [FunctionName("ExecuteSilkCSharp_Hello")]
+        public static string SayHello([ActivityTrigger] string name, ILogger log)
+        {
+            log.LogInformation($"Saying hello to {name}.");
+            return $"Hello {name}!";
+        }
+
+        [FunctionName("ExecuteSilkCSharp_HttpStart")]
+        public static async Task<HttpResponseMessage> HttpStart(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
+            [DurableClient] IDurableOrchestrationClient starter,
+            ILogger log)
+        {
+            // Function input comes from the request content.
+            string instanceId = await starter.StartNewAsync("ExecuteSilkCSharp", null);
+
+            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+
+            return starter.CreateCheckStatusResponse(req, instanceId);
+        }
+    }
+}
