e50e986007a6f8328f46e4f791c957e37b469d1d
Cody Tilkins <deocojo1@gmail.com>
2022-11-17 16:40:59 -0500

    Add extensions for alBufferData for FloatFormat
    - Avoids explicit cast FloatBufferFormat -> BufferFormat

--- src/OpenAL/Extensions/Silk.NET.OpenAL.Extensions.EXT/FloatFormat.cs
+++ src/OpenAL/Extensions/Silk.NET.OpenAL.Extensions.EXT/FloatFormat.cs
@@ -20,4 +20,22 @@ namespace Silk.NET.OpenAL.Extensions.EXT
         {
         }
     }
-}
+
+    /// <summary>
+    /// Extends the methods which make use of FloatBufferFormat.
+    /// </summary>
+    public static class FloatFormatALExtensions
+    {
+        /// <inheritdoc />
+        public unsafe static void BufferData(this AL al, uint buffer, FloatBufferFormat format, void* data, int size, int frequency)
+        {
+            return al.BufferData(buffer, (BufferFormat) format, data, size, frequency);
+        }
+
+        /// <inheritdoc />
+        public static void BufferData<TElement>(this AL al, uint buffer, FloatBufferFormat format, TElement[] data, int frequency)
+            where TElement : unmanaged
+        {
+            return al.BufferData<TElement>(buffer, (BufferFormat) format, data, frequency);
+        }
+    }
